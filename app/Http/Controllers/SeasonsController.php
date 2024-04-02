9d28e0a5ad773fd83d55ab03f1d32900abc8e999
Elvio A. Amaral <elvioamaral.ea@gmail.com>
2024-04-02 08:33:24 -0300

    Add gitignore and remove comments from app/Http/Controllers/SeasonsController.php file.

--- app/Http/Controllers/SeasonsController.php
+++ app/Http/Controllers/SeasonsController.php
@@ -14,25 +14,3 @@ class SeasonsController extends Controller
 
     }
 }
-
-/*
-Passo a passo
-1
-Começando com Laravel
-Neste primeiro passo, você vai aprender a utilizar Laravel. Vamos abordar desde MVC, formulários até a criação de relacionamentos.
-
-
-Concluído
-VIDEO
-Ecossistema PHP com Vinicius Dias | #HipstersPontoTube - YouTube
-
-Concluído
-CURSO
-Laravel: criando uma aplicação com MVC
-
-Concluído
-CURSO
-Laravel: validando formulários, usando sessões e definindo relacionamentos
-
-Formação Laravel da Alura: crie aplicações web em PHP. Cursos já finalizados até a criação desse repo: Laravel - criando uma aplicação com MVC; Laravel - validando formulários, usando sessões e definindo relacionamentos.
- */
