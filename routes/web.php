ca29fa1e11b8d31c25e7bc37b46aa8337fad3e5a
Elvio A. Amaral <elvioamaral.ea@gmail.com>
2024-10-14 08:32:13 -0300

    FORMAÇÃO LARAVEL. CURSO - Laravel: transações, service container e autenticação. Modulo 5: Botões corretos, Aplicando o Mddleware e Breeze (todo o Modulo, com exceção da implementação do Breeze). FIM DESSE CURSO

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\UsersController;
-use App\Http\Middleware\Autenticador;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,17 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/series');
-})->middleware(Autenticador::class);
-
 Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
-Route::get('series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+Route::middleware('autenticador')->group(function () {
+    Route::get('/', function () {
+        return redirect('/series');
+    });
 
-Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+    Route::get('series/{series}/seasons', [SeasonsController::class, 'index'])
+        ->name('seasons.index');
+
+    Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+    Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+
+});
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('signin');
